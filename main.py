e004f34c426d714cb325ef4f11cb5823b2db9650
amirul_islam <amirul.islam@vivacomsolutions.com>
2024-08-05 06:29:21 +0600

    clean file

--- main.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env file
-load_dotenv()
-
-endpoint = os.environ["AZURE_OPENAI_ENDPOINT"]
-deployment = os.environ["CHAT_COMPLETIONS_DEPLOYMENT_NAME"]
