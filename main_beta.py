7450ccc6812605c7947d2062610692928a318646
amirul_islam <amirul.islam@vivacomsolutions.com>
2024-08-06 15:27:12 +0600

    main_beta code completed

--- main_beta.py
+++ main_beta.py
@@ -1,6 +1,11 @@
-from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_openai import AzureChatOpenAI, AzureOpenAI
-from langchain_core.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
+from langchain_openai import AzureChatOpenAI
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.chains.conversation.base import ConversationChain
 
 from dotenv import load_dotenv
 import os
@@ -18,10 +23,29 @@ chat = AzureChatOpenAI(
     azure_deployment=deployment,
     api_version="2024-02-01",
 )
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages",
+    return_messages=True,
+)
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ],
+)
 
-memory = ConversationBufferWindowMemory()
+chain = ConversationChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory,
+    # verbose=True
+)
 
-# We can also specify 'k' parameter here. `k` means last how many chat we want to save
-# memory = ConversationBufferWindowMemory(k=2)
+while True:
+    content = input(">> ")
 
-conversation = ConversationChain(llm=chat, memory=memory, verbose=True)
+    result = chain({"content": content})
+
+    print(result["text"])
