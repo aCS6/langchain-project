27b04493af8642259df488a9cb069733cc8cb73c
amirul_islam <amirul.islam@vivacomsolutions.com>
2024-08-07 13:38:20 +0600

    [APP-03] finalized

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from langchain.prompts import (
-    MessagesPlaceholder,
-    HumanMessagePromptTemplate,
-    ChatPromptTemplate,
-)
+# First Run the seeder.py file manually for first time
 from langchain_openai import AzureChatOpenAI
-from langchain.memory import ConversationSummaryMemory
-from langchain_community.document_loaders import TextLoader
+from langchain import hub
+from langchain_community.vectorstores import Chroma
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+# from langchain.chains.retrieval_qa.base import RetrievalQA
 
 # Has depreciation warning
 from langchain.chains.llm import LLMChain
 
+from seeder import embeddings, DB_PERSIST_DIRECTORY
+from redundant_filter_receiver import RedundantFilterRetriever
+
 from dotenv import load_dotenv
 import os
+
+import langchain
+
+langchain.debug = True
 
 # Load environment variables from .env file
 load_dotenv()
@@ -27,7 +34,12 @@ chat = AzureChatOpenAI(
     api_version="2024-02-01",
 )
 
-loader = TextLoader("facts.txt")
-docs = loader.load()
+db = Chroma(persist_directory=DB_PERSIST_DIRECTORY, embedding_function=embeddings)
+retriever = RedundantFilterRetriever(embeddings=embeddings, chroma=db)
 
-print(docs)
+retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+
+combine_docs_chain = create_stuff_documents_chain(chat, retrieval_qa_chat_prompt)
+retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
+
+result = retrieval_chain.invoke("Tell me something about Bangladesh?")
---
+++ seeder.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+
+load_dotenv()
+
+endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
+deployment = os.environ.get("CHAT_COMPLETIONS_DEPLOYMENT_NAME")
+api_key = os.environ.get("API_KEY")
+
+embeddings = AzureOpenAIEmbeddings(
+    openai_api_key=api_key,
+    openai_api_type="azure",
+    azure_endpoint=endpoint,
+    openai_api_version="2024-02-01",
+    model="text-embedding-ada-002",
+)
+DB_PERSIST_DIRECTORY = "emb_db"
+
+
+def seed():
+
+    if not os.path.exists(DB_PERSIST_DIRECTORY):
+        text_splitter = CharacterTextSplitter(
+            separator="\n", chunk_size=200, chunk_overlap=0
+        )
+
+        loader = TextLoader("facts.txt")
+        docs = loader.load_and_split(text_splitter=text_splitter)
+
+        db = Chroma.from_documents(
+            docs, embedding=embeddings, persist_directory=DB_PERSIST_DIRECTORY
+        )
+
+        # test the db
+        results = db.similarity_search("Where is the largest railway networks?")
+
+        for result in results:
+            print("\n")
+            print(result.page_content)
+
+    print("Successfully Seeded!")
+
+
+if __name__ == "__main__":
+    seed()
